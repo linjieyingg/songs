21b811129c11cecef9efdd4d184cd92390be00b3
Jieying Lin <jlin41@stuy.edu>
2024-01-08 14:44:53 -0500

    a

--- mimuse/artists/models.py
+++ mimuse/artists/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Artist(models.Model):
-    api_id = models.CharField()
+    api_id = models.CharField(null=True)
     name = models.CharField(max_length=100, unique=True)
     overview = models.CharField()
     popularity = models.SmallIntegerField()
--- mimuse/songs/models.py
+++ mimuse/songs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from artists.models import Artist
 
 class Album(models.Model):
-    api_id = models.CharField()
+    api_id = models.CharField(null=True)
     title = models.CharField(max_length=50)
     release_date = models.DateField()
     
@@ -12,7 +12,7 @@ class Album(models.Model):
         ordering = ['title']
     
 class Song(models.Model):
-    api_id = models.CharField()
+    api_id = models.CharField(null=True)
     title = models.CharField(max_length=50)
     album_id = models.ForeignKey(Album, null=True, on_delete=models.SET_NULL)
     artists = models.ManyToManyField(Artist)
