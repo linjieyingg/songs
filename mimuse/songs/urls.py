abca7e5eae12211c3165251cb77ed2df92c146d7
xruan40 <xruan40@stuy.edu>
2024-01-09 17:25:01 -0500

    x

--- mimuse/songs/urls.py
+++ mimuse/songs/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = "songs"
 
 urlpatterns = [
+    path('', views.SongListView.as_view(), name="song_list"),
+    # path("<int:pk>", views.ArtistDetailView.as_view(), name="artist_detail")
 ]
