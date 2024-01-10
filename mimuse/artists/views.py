7885767231f6ce20b6e814d9c4d2df6a93a0037d
Jieying Lin <jlin41@stuy.edu>
2024-01-10 14:54:25 -0500

    s

--- mimuse/artists/views.py
+++ mimuse/artists/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 # from django.urls import reverse_lazy
 # from django.contrib import messages
 from .models import Artist
+from songs.models import Song, Album
 # Create your views here.
 
 class ArtistListView(ListView):
@@ -11,4 +12,11 @@ class ArtistListView(ListView):
     
 class ArtistDetailView(DetailView):
     model = Artist
-    
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context["song_list"] = Song.objects.all()
+        return context
+
