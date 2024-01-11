e0b937c28f3177e1d0f8deb1ec429b2529943ca7
reyamiller <72947255+reyamiller@users.noreply.github.com>
2024-01-10 19:55:23 -0500

    cw from today

--- mimuse/songs/views.py
+++ mimuse/songs/views.py
@@ -3,11 +3,14 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 # from django.urls import reverse_lazy
 # from django.contrib import messages
-from .models import Song
+from .models import Album, Song
 # Create your views here.
 
 class SongListView(ListView):
     model = Song
+    
+class AlbumListView(ListView):
+    model = Album
     
 # class ArtistDetailView(DetailView):
 #     model = Artist
