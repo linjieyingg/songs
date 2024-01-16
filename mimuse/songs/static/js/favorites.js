af58e114e45da2eff6db6d53c6aa1e8987275dbc
reyamiller <72947255+reyamiller@users.noreply.github.com>
2024-01-15 22:21:13 -0500

    continue working on changing favorite value

--- mimuse/songs/static/js/favorites.js
+++ mimuse/songs/static/js/favorites.js
@@ -1,8 +1,18 @@
 import Song from models;
 
-$('#favorite').click(function() {
+$("#favorite").onclick(function() {
     var song = Song.objects.get(api_id=id);
     if (song.favorite == False) song.favorite = True;
     else song.favorite = False;
     song.save();
+    alert("Favorites edited.");
 });
+
+$(".btn btn-primary").onclick = function() {
+    var id = this.id;
+    var song = Song.objects.get(api_id=id);
+    if (song.favorite == False) song.favorite = True;
+    else song.favorite = False;
+    song.save();
+    alert("Favorites edited.");
+};
